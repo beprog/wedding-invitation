bd552d552211a44ffd109f7c9e2a565fae479738
beprog <beprog@gmail.com>
2025-12-22 07:20:47 +0900

    github link remove

--- src/components/sections/Footer - 복사본.tsx
+++ src/components/sections/Footer - 복사본.tsx
@@ -19,6 +19,13 @@ const Footer = () => {
       <FooterContent>
         <Copyright>© {currentYear} Jawon Koo</Copyright>
         <Credits>Made with ❤️</Credits>
+        <GithubLink
+          href="https://github.com/jw-koo/wedding-invitation"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub 저장소 바로가기
+        </GithubLink>
         <HiddenAttribution data-jwk-id={watermarkId}>
           NonCommercial
         </HiddenAttribution>
