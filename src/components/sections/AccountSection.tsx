1323ae4c8be3dd3676a473ecb3e6bc97c010a65f
beprog <beprog@gmail.com>
2025-12-22 01:02:13 +0900

    bank account t4

--- src/components/sections/AccountSection.tsx
+++ src/components/sections/AccountSection.tsx
@@ -155,7 +155,6 @@ const AccountSection = ({ bgColor = 'white' }: AccountSectionProps) => {
             <AccountRowsContainer>
               {renderAccountRow(weddingConfig.account.groom, 'groom', '신랑')}
               {renderAccountRow(weddingConfig.account.groomFather, 'groomFather', '아버지')}
-              {renderAccountRow(weddingConfig.account.groomMother, 'groomMother', '어머니')}
             </AccountRowsContainer>
           )}
         </AccountCard>
@@ -172,7 +171,6 @@ const AccountSection = ({ bgColor = 'white' }: AccountSectionProps) => {
           {expandedSide === 'bride' && (
             <AccountRowsContainer>
               {renderAccountRow(weddingConfig.account.bride, 'bride', '신부')}
-              {renderAccountRow(weddingConfig.account.brideFather, 'brideFather', '아버지')}
               {renderAccountRow(weddingConfig.account.brideMother, 'brideMother', '어머니')}
             </AccountRowsContainer>
           )}
